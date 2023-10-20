f746e176b4572101264b4c8ef58480451c9c53d9
User2 <notescape262@gmail.com>
2023-10-20 13:32:28 +0200

    Добавьте файлы проекта.

---
+++ lab5rpm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static int ConvertToSeconds(int hours, int minutes)
+    {
+        if (hours < 0 || minutes < 0 || minutes >= 60)
+        {
+            throw new ArgumentException("Недопустимые значения часов и минут.");
+        }
+
+        int seconds = (hours * 3600) + (minutes * 60);
+        return seconds;
+    }
+
+    static void Main()
+    {
+        Console.Write("Write hourse: ");
+        int hours = Int32.Parse(Console.ReadLine());
+        Console.Write("Write minutes: ");
+        int minutes = Int32.Parse(Console.ReadLine());
+
+        int seconds = ConvertToSeconds(hours, minutes);
+        Console.WriteLine($"{hours} часа(ов) и {minutes} минут(ы) составляют {seconds} секунд.");
+    }
+}
